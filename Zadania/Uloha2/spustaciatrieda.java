af5e97666c5e61577346065b39a23ece64465144
TreaTeT <47723558+TreaTeT@users.noreply.github.com>
2019-03-05 19:26:52 +0100

    Update spustaciatrieda.java

--- Zadania/Uloha2/spustaciatrieda.java
+++ Zadania/Uloha2/spustaciatrieda.java
@@ -5,15 +5,14 @@ import java.io.IOException;
 public class spustaciatrieda {
     public static void main(String[] args) throws IOException {
         Pole a = new Pole(4,4);
-        //a.klavesnica();
-        //a.najdi('a');
-        a.subor();
-       // a.vymenRiadky(1,2);
-       // a.vypis();
-        a.vymenStlpce(1,2);
+        //a.klavesnica(); nacitanie z klavesnice
+        //a.najdi('a'); najdenie zadaneho charu v poli
+        a.subor(); //nacitanie zo suboru
+        a.vymenRiadky(1,2); //vymenenie riadkov
+        a.vymenStlpce(1,2); //vymenenie stlpcov
         System.out.println();
-        a.vypis();
-        a.zapis();
+        a.vypis(); //vypis na konzolu
+        a.zapis(); //zapis do suboru
 
     }
 }
