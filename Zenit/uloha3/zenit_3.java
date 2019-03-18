132aa0d24dfe698580e7e9a2f188be805fb89d36
Filo420 <48220916+Filo420@users.noreply.github.com>
2019-03-18 12:46:32 +0100

    Add files via upload

---
+++ Zenit/uloha3/zenit_3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class zenit_3 {
+
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());            // nacitanie poctu slov
+        String[] x = in.readLine().split(" ");        //nacitanie jednotlivycj slov
+
+        for (int i = 0; i < N; i++){
+            for (int j = x[i].length() - 1; j >= 0; j--){
+                System.out.print(x[i].charAt(j));          //vypisovanie uz otoceniych slov
+            }
+            System.out.print(" ");
+        }
+
+    }
+}
