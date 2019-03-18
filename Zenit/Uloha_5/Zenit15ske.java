8b520602e32dc40d35da35720beff491988f138e
TreaTeT <treatett@centrum.sk>
2019-03-18 19:34:27 +0100

    adding zenit5

---
+++ Zenit/Uloha_5/Zenit15ske.java
@@ -0,0 +1,35 @@
+package Skola.Zenit.Uloha_5;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.TreeMap;
+public class Zenit15ske {
+    public static void main(String[] args) throws IOException {
+        Map<String, Integer> map = new TreeMap<String, Integer>(); //spravim si map
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        in.readLine(); //prvy riadok je cislo pocet slov vo vete
+        String[] arr = in.readLine().split(" "); //druhy riadok veta
+
+        for (String word : arr) { //for each
+            if (map.containsKey(word)) { //ak v mape mam toto slovo
+                map.put(word, map.get(word) + 1); //pripocitam k jeho value +1
+            } else {
+                map.put(word, 1); //inak vytvorim novy zapis do mapu
+            }
+        }
+        if (((TreeMap<String, Integer>) map).lastEntry().getValue() == 1) { //tree map hned zoraduje takze ak last je 1 nijake slovo sa neopakuje
+            System.out.println(((TreeMap<String, Integer>) map).firstEntry().getKey()); //vypisem prve a teda najmensie slovo
+        } else {
+            int max = ((TreeMap<String, Integer>) map).lastEntry().getValue(); //max je posledna cize max hodnota v mape (max pocet opakovani slova)
+            for (Map.Entry<String, Integer> entry : map.entrySet()) { //for each cez map
+                if(entry.getValue()==max){ //ak sa ich opakuje viac chceme to z najmensou hodnotou
+                    System.out.println(entry.getKey()); // vypisem to z najmensou hodnotou ktore sa opakuje max krat
+                    break; //zlomim ti ruku
+                }
+
+            }
+
+        }
+    }
+}
