4b0fc70271e5e0e54f8034a749f9e3f0e07a97f9
NewPower9 <48221263+NewPower9@users.noreply.github.com>
2019-03-25 18:50:03 +0100

    Add files via upload

---
+++ Zadania/Uloha1/Spustenie.java
@@ -0,0 +1,27 @@
+package Uloha1;
+
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Spustenie {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Riadky:");
+        int r=sc.nextInt();
+        System.out.println("Stlpce:");
+        int s=sc.nextInt();
+        Hladanie x = new Hladanie(r,s);
+        x.nacitanieK();
+        x.nacitanieS();
+        x.Max();
+        System.out.println("Max do suboru");
+        x.vypisS(sc.nextInt(),sc.nextInt());
+        System.out.println("Max v riadkoch a stlpcoch");
+        x.maR(r);
+        x.maS(s);
+        x.vMaxR(sc.nextInt());
+        x.vMaxS(sc.nextInt());
+
+    }
+}
