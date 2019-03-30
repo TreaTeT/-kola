ce3782744d2e6a47c555af4c3fc49fc100651c69
Filo420 <48220916+Filo420@users.noreply.github.com>
2019-03-30 13:56:46 +0100

    Add files via upload

---
+++ Zadania/Uloha3/pole.java
@@ -0,0 +1,103 @@
+package Uloha_3;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class pole {
+
+    private int stlpce;
+    private int riadky;
+    private String[][] B;
+
+    public pole(int riadky, int stlpce){ //konstruktor
+        this.B = new String[riadky][stlpce];
+        this.riadky = riadky;
+        this.stlpce = stlpce;
+    }
+
+    public void konzolaVstup(){         //nacitanie pola pomocou klavesnice
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadavaj do pola");
+        for (int i = 0; i < this.B.length; i++){
+            for (int j = 0; j < this.B[i].length; i++){
+                this.B[i][j] = sc.nextLine();
+            }
+        }
+    }
+    public void konzolaVypis(){     //vypis pola na konzolu
+        for(int i = 0 ; i < this.B.length; i++){
+            for(int j = 0 ; j < this.B[i].length;j++){
+                System.out.print(this.B[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void suborVstup() throws IOException {   //nacitanie pola pomocou suboru
+        BufferedReader input = new BufferedReader(new FileReader("vstupPole.txt"));
+        for (int i = 0; i < this.riadky; i++){
+                this.B[i] = input.readLine().split(" ");
+        }
+        input.close();
+    }
+    public void suborVystup() throws IOException {  //vypis pola do suboru
+        BufferedWriter output = new BufferedWriter(new FileWriter("vypisPole.txt",true));
+        for (int i = 0; i < this.riadky; i++){
+            for (int j = 0; j < this.stlpce; j++){
+                output.write(this.B[i][j] + " ");
+            }
+            output.newLine();
+        }
+        output.newLine();
+        output.close();
+    }
+
+    public void zoradRiadok(int riadok) {           //zoradenie urciteho riadka pomocou bubble sortu
+        for (int i = 0; i < this.B.length - 1; i++) {
+            for (int j = 0; j < this.B.length - i - 1; j++) {
+                if (this.B[riadok][j].compareTo(this.B[riadok][j + 1]) > 0) {
+                    String docasny = this.B[riadok][j];
+                    this.B[riadok][j] = this.B[riadok][j + 1];
+                    this.B[riadok][j + 1] = docasny;
+                }
+            }
+        }
+    }
+
+    public void zoradStplec(int stlpec){        //zoradenie urciteho stlpca pomocou insertion sortu
+        for (int i = 0; i < this.stlpce - 1; i++) {
+            int j = i + 1;
+            String docasna = this.B[j][stlpec];
+            while (j > 0 && this.B[j - 1][stlpec].compareTo(docasna) > 0) {
+                this.B[j][stlpec] = this.B[j - 1][stlpec];
+                j--;
+            }
+            this.B[j][stlpec] = docasna;
+        }
+    }
+
+
+    public void zoradHlavnuDiagonalu(){         //zoradenie hlavnej diagonaly pomocou selection sortu
+        for (int i = 0; i < this.B.length - 1; i++) {
+            int maxIndex = i;
+            for (int j = i + 1; j < this.B.length; j++) {
+                if (this.B[j][j].compareTo(this.B[maxIndex][maxIndex]) < 1) maxIndex = j;
+            }
+            String tmp = this.B[i][i];
+            this.B[i][i] = this.B[maxIndex][maxIndex];
+            this.B[maxIndex][maxIndex] = tmp;
+        }
+    }
+    public void zoradVedlajsiuDiagonalu() {         //zoradenie vedlajsej diagonaly pomocou selection sortu
+        for (int i = 0; i < this.B.length - 1; i++) {       //bez pouzitia pomocneho pola!!!!
+            int maxIndex = i;
+            for (int j = i + 1; j < this.B.length; j++) {
+                if (this.B[j][this.B.length - j - 1].compareTo(this.B[maxIndex][this.B.length - maxIndex - 1]) < 1)
+                    maxIndex = j;
+            }
+            String tmp = this.B[i][this.B.length - i - 1];
+            this.B[i][this.B.length - i - 1] = this.B[maxIndex][this.B.length - maxIndex - 1];
+            this.B[maxIndex][this.B.length - maxIndex - 1] = tmp;
+        }
+}
+}
---
+++ Zadania/Uloha3/spust.java
@@ -0,0 +1,35 @@
+package Uloha_3;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class spust {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter output = new BufferedWriter(new FileWriter("vypisPole.txt")); //tento bufferedwriter je len na premazanie suboru
+        Scanner sc = new Scanner(System.in);                                                 //aby sme nemali milion poli vypisanych v subore
+        pole pole_a = new pole(4,4);
+        pole_a.suborVstup();
+        System.out.println("Zadaj ktory riadok chces zoradit");
+        pole_a.zoradRiadok(sc.nextInt());       //zoradienie riadka a vypis pola
+        pole_a.konzolaVypis();
+        pole_a.suborVystup();
+        System.out.println("Zadaj ktory stlpec chces zoradit");
+        pole_a.zoradStplec(sc.nextInt());       //zoradienie stlpca a vypis pola
+        pole_a.konzolaVypis();
+        pole_a.suborVystup();
+        System.out.println();
+        pole_a.zoradHlavnuDiagonalu();          //zoradienie hlavnej diagonaly a vypis pola
+        pole_a.konzolaVypis();
+        pole_a.suborVystup();
+        System.out.println();
+        pole_a.zoradVedlajsiuDiagonalu();       //zoradienie vedlajsej diagonaly a vypis pola
+        pole_a.konzolaVypis();
+        pole_a.suborVystup();
+
+
+    }
+
+}
