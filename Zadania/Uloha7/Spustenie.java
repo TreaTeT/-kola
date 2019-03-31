a7df4bb0acb6d130e89463a1b7fb08305e920aaa
NewPower9 <48221263+NewPower9@users.noreply.github.com>
2019-03-31 13:36:10 +0200

    Add files via upload

---
+++ Zadania/Uloha7/Spustenie.java
@@ -0,0 +1,12 @@
+package Uloha7;
+
+import java.io.*;
+
+public class Spustenie {
+    public static void main(String[] args) throws IOException{
+        Zoznam x = new Zoznam();
+            x.citaj();
+            x.vypis();
+
+    }
+ }
---
+++ Zadania/Uloha7/Zoznam.java
@@ -0,0 +1,37 @@
+package Uloha7;
+
+import java.io.*;
+
+public class Zoznam {
+    private String [] A;
+
+    public void citaj() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("D:\\udajeC.txt"));
+        A = br.readLine().split(" ");
+    }
+    public boolean jeToMuz(String cislo){
+        if(Integer.parseInt(cislo.substring(2,4))-50 > 0)
+            return false;
+        else
+            return true;
+    }
+    public String vypocet(){
+        int muzi = 0;
+        int zeny = 0;
+        for(int i=0; i<A.length; i++){
+            if(jeToMuz(A[i]) == true)
+                muzi ++;
+            else
+                zeny ++;
+        }
+        return "Pocet muzov: "+muzi+" Pocet zien: "+zeny;
+    }
+    public void vypis() throws IOException{
+        BufferedWriter bw = new BufferedWriter(new FileWriter("D:\\vystupC.txt"));
+        bw.write(vypocet());
+        bw.close();
+        System.out.println("Hotovo");
+    }
+
+}
+
