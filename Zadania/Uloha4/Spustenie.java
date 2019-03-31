eb09b4a203b802ea7a0398755d5a7f795b749229
NewPower9 <48221263+NewPower9@users.noreply.github.com>
2019-03-31 13:35:08 +0200

    Add files via upload

---
+++ Zadania/Uloha4/Spustenie.java
@@ -0,0 +1,19 @@
+package Uloha4;
+
+import java.io.*;
+
+public class Spustenie {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new FileReader("D:\\ziaci.txt"));
+            int pocetziakov = 0;
+            while(br.readLine() != null){
+                pocetziakov ++;
+            }
+            br.close();
+            Ziaci SA = new Ziaci(pocetziakov);
+            SA.nacitanie();
+            SA.AP();
+            SA.usporiadanie();
+            SA.vypis();
+        }
+}
