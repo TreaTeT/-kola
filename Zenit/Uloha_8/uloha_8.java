1e60ceabc77e2ae5ebc50a67846dc4b18ea9a1a4
TreaTeT <treatett@centrum.sk>
2019-04-20 13:55:31 +0200

    adding zenit skh moja verzia

--- Zenit/Uloha_8/uloha_8.java
+++ Zenit/Uloha_8/uloha_8.java
@@ -5,36 +5,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class uloha_8 {
-    static int low[] = {0, 5, 3, 3, 5, 3, 4, 5, 4, 5, 5, 8, 7, 7, 7, 7, 8, 9, 8, 9};
-    static int tens[] = {0, 5, 7, 7, 9, 9, 10, 11, 10, 11};
-
-    public static int toThousand(int value){
-        int res = 0;
-        int h = value/100;
-        if(h > 1) {
-            res = res + low[h];
-        }
-        if(h>0) {
-            res = res + 3 ; // sto
-        }
-        int lo = value%100;
-        if(lo < 20) {
-            res = res + low[lo];
-        }else {
-            res = res + tens[lo/10] + low[lo%10];
-        }
-        return res;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(in.readLine());
-        int res = toThousand(N%1000);
-        if(N >= 1000){
-            res = res + 5; //1000
+        int number = Integer.parseInt(in.readLine()); // nacitaj cislo
+        int value  = 0 ; // pocet pismen
+        int unitsArray[] = {0, 5, 3, 3, 5, 3, 4, 5, 4, 5, 5, 8, 7, 7, 7, 7, 8, 9, 8, 9 }; //jednotky 0-19
+        int tensArray[] = { 0, 5, 7, 7, 9, 9, 10, 11, 10, 11 }; //desiatky 10-90
+
+        int [] arr = {number/1000 , number%1000};  //rozdelim cislo na dva 11999 => [11] [999]
+
+        for(int num  : arr){
+            if(num/100 > 1){
+                value = value + unitsArray[num/100];  //kolko stoviek je v cisle 999 => [9]99
+            }
+            if(num/100 >0){
+                value = value + 3;  // ak je iba jedna tak pricotiam len sto
+            }
+            if(num%100 < 20){ // ak je tato cast cisla 9[99] mensia ako 20
+                value = value + unitsArray[num%100];  // pripocitam z jednotiek
+            }else{
+                value = value + tensArray[(num%100)/10] + unitsArray[(num%100)%10];  // pripocitam z desiatok
+            }
         }
-        if(N >= 2000){
-            res = res + toThousand(N/1000);
+        if(arr[0]> 0){ //ak je prve cislo vacsie ako 0 znamena to ze mame nejake tisicky tak pripocitam 5
+            value = value + 5; // 5 => tisic
         }
-        System.out.println(res);
+        System.out.println(value);
+
+
     }
 }
